3129c169c768a61fb8cbfb76973692f0d10d5dc7
Orxan Huseynov <115917125+Orxan044@users.noreply.github.com>
2023-04-06 16:43:20 +0400

    Add project files.

---
+++ ders1/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders1
+{
+    internal class Group
+    {
+
+        public Teacher teacher{ get; set; }
+        public List<Student> students{ get; set; }
+    }
+}
---
+++ ders1/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders1
+{
+
+    enum SchrolShep { verygood,middle,standart}
+
+    internal class Student
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public DateTime? Age { get; set; }
+
+        public double Avarge { get; set; }
+
+        public SchrolShep schrolShep { get; set; }
+
+    }
+}
